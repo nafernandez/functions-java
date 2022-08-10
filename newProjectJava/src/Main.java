2761c3061e32d1494b2b18778e41739a06f1f10a
nafernandez <nataliahernda@gmail.com>
2022-08-10 18:50:15 -0300

    First exercise

--- newProjectJava/src/Main.java
+++ newProjectJava/src/Main.java
@@ -1,15 +1,30 @@
 public class Main {
     public static void main(String[] args) {
 
-        suma(10, 30);
-        //System.out.println(a);
+        //primera parte
+
+        suma(10, 20, 30);
+
+        //segunda parte
+
+        Coche miCoche = new Coche();
+        miCoche.agregarPuertas();
+        miCoche.mostrarNumeroPuertas();
     }
 
-    public static void suma(int a, int b) {
-        System.out.println(a + b);
-        System.out.println(a - b);
-        System.out.println(a * b);
-        System.out.println(a / b);
+    public static void suma(int a, int b, int c) {
+        System.out.println(a + b + c);
+    }
+}
 
+class Coche {
+    public int puertas = 0;
+
+    public void agregarPuertas() {
+        this.puertas ++;
+    }
+
+    public void mostrarNumeroPuertas() {
+        System.out.println(this.puertas);
     }
 }
