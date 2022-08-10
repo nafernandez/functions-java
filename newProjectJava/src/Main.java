3199e6f69fff95b2e09904a4c052da32489a82b9
nafernandez <natyfh0798@gmail.com>
2022-08-10 17:39:24 -0300

    First commit

---
+++ newProjectJava/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        suma(10, 30);
+        //System.out.println(a);
+    }
+
+    public static void suma(int a, int b) {
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+
+    }
+}
